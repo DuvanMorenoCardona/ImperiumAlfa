b04824c7dc26aae9b168bda06219e828a0d4d7bf
ocralo <e_lopez1996@hotmail.com>
2019-03-11 00:53:13 -0500

    se crea el menu hamburguesa en monitores

--- app/js/monitores.js
+++ app/js/monitores.js
@@ -1,31 +1,9 @@
-if (window.File && window.FileReader && window.FileList && window.Blob) {
-  // Great success! All the File APIs are supported.
-} else {
-  alert("The File APIs are not fully supported in this browser.");
-}
-
-function handleFileSelect(evt) {
-  var files = evt.target.files; // FileList object
-
-  // files is a FileList of File objects. List some properties.
-  var output = [];
-  for (var i = 0, f; (f = files[i]); i++) {
-    output.push(
-      "<li><strong>",
-      escape(f.name),
-      "</strong> (",
-      f.type || "n/a",
-      ") - ",
-      f.size,
-      " bytes, last modified: ",
-      f.lastModifiedDate.toLocaleDateString(),
-      "</li>"
-    );
-  }
-  document.getElementById("list").innerHTML =
-    "<ul>" + output.join("") + "</ul>";
-}
-
-document
-  .getElementById("files")
-  .addEventListener("change", handleFileSelect, false);
+$(document).ready(function () {
+  var sideslider = $('[data-toggle=collapse-side]');
+  var sel = sideslider.attr('data-target');
+  var sel2 = sideslider.attr('data-target-2');
+  sideslider.click(function (event) {
+    $(sel).toggleClass('in');
+    $(sel2).toggleClass('out');
+  });
+});
